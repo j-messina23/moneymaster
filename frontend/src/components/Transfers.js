534a601eb66bc8c1bb4c2d72f0c0b47c3b31096e
tchalfpenny <135466340+tchalfpenny@users.noreply.github.com>
2024-04-11 19:09:24 -0400

    Update Transfers.js
    Final CSS change.

--- frontend/src/components/Transfers.js
+++ frontend/src/components/Transfers.js
@@ -74,13 +74,15 @@ function Transfers({ isPanelExpanded }) {
             let txt = await response.text();
             let res = JSON.parse(txt);
             console.log(res.balance);
-            if (transferAmount <= res.balance) {
+            if ((transferAmount <= res.balance) && (transferAmount <= 500)) {
                 console.log('Balance valid.')
                 CompleteTransfer(event);
                 setTransferSuccess('True')
                 setTransferMessage(transferAmount + ' sent to ' + transferTarget + ' successfully!')
-            } else {
+            } else if ((transferAmount > res.balance) && (transferAmount <= 500)) {
                 console.log('Balance invalid.')
+            } else {
+                console.log('Single Transfer Limit Exceeded.')
             }
         }
         catch (e) {
@@ -95,22 +97,33 @@ function Transfers({ isPanelExpanded }) {
         console.log(transferAmount)
         let obj = { "UserID": UserID, "Type": transferAccountType, "Money": transferAmount };
         let js = JSON.stringify(obj);
-        try {
-            const response = await
-                fetch(buildPath('api/transferMoneyAccount'),
-                    {
-                        method: 'POST', body: js, headers: {
-                            'Content-Type':
-                                'application/json'
-                        }
-                    });
-            let txt = await response.text();
-            let res = JSON.parse(txt);
-            console.log(res)
+        if (transferAmount <= 500) {
+            try {
+                const response = await
+                    fetch(buildPath('api/transferMoneyAccount'),
+                        {
+                            method: 'POST', body: js, headers: {
+                                'Content-Type':
+                                    'application/json'
+                            }
+                        });
+                let txt = await response.text();
+                let res = JSON.parse(txt);
+                console.log(res)
+                setTransferSuccess('True')
+                if (transferAccountType === '1') {
+                    setTransferMessage('$' + transferAmount + ' sent from Checking to Savings!')
+                } else if (transferAccountType === '2') {
+                    setTransferMessage('$' + transferAmount + ' sent from Savings to Checking!')
+                }
+            }
+            catch (e) {
+                console.log(e.toString())
+            }
+        } else {
+            console.log('Single Transfer Limit Exceeded.')
         }
-        catch (e) {
-            console.log(e.toString())
-        }
+
     }
 
     function AccountsHref() {
@@ -132,50 +145,22 @@ function Transfers({ isPanelExpanded }) {
                 <div className="w-3/6 h-3/6 outline rounded outline-offset-4 outline-white">
                     <button onClick={selectForm} className="bg-white text-teal-800 font-semibold py-2 px-4 mr-2 rounded hover:bg-gray-200 items-center justify-center">Toggle Form</button>
                     {switchForms ? (
-                        <div className="space-y-3">
-                            <div className="relative w-full min-w-[200px] h-10">
-                                <input
-                                    value={transferTarget}
-                                    onChange={e => setTransferTarget(e.target.value)}
-                                    className="peer w-full h-full bg-transparent text-white font-normal outline outline-0 focus:outline-0 disabled:bg-blue-gray-50 disabled:border-0 transition-all placeholder-shown:border placeholder-shown:border-white placeholder-shown:border-t-blue-gray-200 border focus:border-2 border-t-transparent focus:border-t-transparent text-sm px-3 py-2.5 rounded-[7px] border-blue-gray-200 focus:border-white"
-                                    placeholder=" "
-                                />
-                                <label
-                                    className="flex w-full h-full select-none pointer-events-none absolute left-0 font-normal !overflow-visible truncate peer-placeholder-shown:text-white leading-tight peer-focus:leading-tight peer-disabled:text-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500 transition-all -top-1.5 peer-placeholder-shown:text-sm text-[11px] peer-focus:text-[11px] before:content[' '] before:block before:box-border before:w-2.5 before:h-1.5 before:mt-[6.5px] before:mr-1 peer-placeholder-shown:before:border-transparent before:rounded-tl-md before:border-t peer-focus:before:border-t-2 before:border-l peer-focus:before:border-l-2 before:pointer-events-none before:transition-all peer-disabled:before:border-transparent after:content[' '] after:block after:flex-grow after:box-border after:w-2.5 after:h-1.5 after:mt-[6.5px] after:ml-1 peer-placeholder-shown:after:border-transparent after:rounded-tr-md after:border-t peer-focus:after:border-t-2 after:border-r peer-focus:after:border-r-2 after:pointer-events-none after:transition-all peer-disabled:after:border-transparent peer-placeholder-shown:leading-[3.75] text-white peer-focus:text-white before:border-blue-gray-200 peer-focus:before:!border-white after:border-blue-gray-200 peer-focus:after:!border-white">Username
-                                </label>
-                            </div>
-                            <div className="relative w-full min-w-[200px] h-10">
-                                <input
-                                    value={transferAmount}
-                                    onChange={e => setTransferAmount(e.target.value)}
-                                    className="peer w-full h-full bg-transparent text-white font-normal outline outline-0 focus:outline-0 disabled:bg-blue-gray-50 disabled:border-0 transition-all placeholder-shown:border placeholder-shown:border-white placeholder-shown:border-t-blue-gray-200 border focus:border-2 border-t-transparent focus:border-t-transparent text-sm px-3 py-2.5 rounded-[7px] border-blue-gray-200 focus:border-white"
-                                    placeholder=" "
-                                />
-                                <label
-                                    className="flex w-full h-full select-none pointer-events-none absolute left-0 font-normal !overflow-visible truncate peer-placeholder-shown:text-white leading-tight peer-focus:leading-tight peer-disabled:text-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500 transition-all -top-1.5 peer-placeholder-shown:text-sm text-[11px] peer-focus:text-[11px] before:content[' '] before:block before:box-border before:w-2.5 before:h-1.5 before:mt-[6.5px] before:mr-1 peer-placeholder-shown:before:border-transparent before:rounded-tl-md before:border-t peer-focus:before:border-t-2 before:border-l peer-focus:before:border-l-2 before:pointer-events-none before:transition-all peer-disabled:before:border-transparent after:content[' '] after:block after:flex-grow after:box-border after:w-2.5 after:h-1.5 after:mt-[6.5px] after:ml-1 peer-placeholder-shown:after:border-transparent after:rounded-tr-md after:border-t peer-focus:after:border-t-2 after:border-r peer-focus:after:border-r-2 after:pointer-events-none after:transition-all peer-disabled:after:border-transparent peer-placeholder-shown:leading-[3.75] text-white peer-focus:text-white before:border-blue-gray-200 peer-focus:before:!border-white after:border-blue-gray-200 peer-focus:after:!border-white">Amount
-                                </label>
-                                <div className="w-fullitems-center flex justify-evenly">
-                                    <button onClick={CheckTransferValidity} className="w-2/5 bg-white text-teal-800 font-semibold py-2 px-4 rounded hover:bg-gray-200">Send</button>
-                                    <button onClick={AccountsHref} className="w-2/5 bg-white text-teal-800 font-semibold py-2 px-4 rounded hover:bg-gray-200">Back To Accounts</button>
-                                </div>
-                            </div>
-                            {transferSuccess==='True' && (
-                                <div class="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative" role="alert">
-                                    <span class="block sm:inline">{transferMessage}</span>
-                                </div>
-                            )}
-                        </div>
-
-                    ) : (
-                        <div>
-                            <div className="space-y-3">
+                        <div className="h-full flex flex-col justify-around">
+                            <div>
+                            <div className="w-full py-3">
                                 <div className="relative w-full min-w-[200px] h-10">
-                                    <Select id="selectAccount"
-                                        options={options}
-                                        onChange={handleChange}
-                                        defaultValue={options[0]}
+                                    <input
+                                        value={transferTarget}
+                                        onChange={e => setTransferTarget(e.target.value)}
+                                        className="peer w-full h-full bg-transparent text-white font-normal outline outline-0 focus:outline-0 disabled:bg-blue-gray-50 disabled:border-0 transition-all placeholder-shown:border placeholder-shown:border-white placeholder-shown:border-t-blue-gray-200 border focus:border-2 border-t-transparent focus:border-t-transparent text-sm px-3 py-2.5 rounded-[7px] border-blue-gray-200 focus:border-white"
+                                        placeholder=" "
                                     />
+                                    <label
+                                        className="flex w-full h-full select-none pointer-events-none absolute left-0 font-normal !overflow-visible truncate peer-placeholder-shown:text-white leading-tight peer-focus:leading-tight peer-disabled:text-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500 transition-all -top-1.5 peer-placeholder-shown:text-sm text-[11px] peer-focus:text-[11px] before:content[' '] before:block before:box-border before:w-2.5 before:h-1.5 before:mt-[6.5px] before:mr-1 peer-placeholder-shown:before:border-transparent before:rounded-tl-md before:border-t peer-focus:before:border-t-2 before:border-l peer-focus:before:border-l-2 before:pointer-events-none before:transition-all peer-disabled:before:border-transparent after:content[' '] after:block after:flex-grow after:box-border after:w-2.5 after:h-1.5 after:mt-[6.5px] after:ml-1 peer-placeholder-shown:after:border-transparent after:rounded-tr-md after:border-t peer-focus:after:border-t-2 after:border-r peer-focus:after:border-r-2 after:pointer-events-none after:transition-all peer-disabled:after:border-transparent peer-placeholder-shown:leading-[3.75] text-white peer-focus:text-white before:border-blue-gray-200 peer-focus:before:!border-white after:border-blue-gray-200 peer-focus:after:!border-white">Username
+                                    </label>
                                 </div>
+                            </div>
+                            <div className="w-full py-3">
                                 <div className="relative w-full min-w-[200px] h-10">
                                     <input
                                         value={transferAmount}
@@ -188,10 +173,52 @@ function Transfers({ isPanelExpanded }) {
                                     </label>
                                 </div>
                             </div>
-                            <div className="w-fullitems-center flex justify-evenly">
-                                <button onClick={TransferUser} className="w-2/5 bg-white text-teal-800 font-semibold py-2 px-4 rounded hover:bg-gray-200">Send</button>
-                                <button onClick={AccountsHref} className="w-2/5 bg-white text-teal-800 font-semibold py-2 px-4 rounded hover:bg-gray-200">Back To Accounts</button>
                             </div>
+                            {transferSuccess === 'True' && (
+                                <div class="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative" role="alert">
+                                    <span class="block sm:inline">{transferMessage}</span>
+                                </div>
+                            )}
+                                <div className="w-full items-center flex justify-evenly py-3">
+                                    <button onClick={CheckTransferValidity} className="w-2/5 bg-white text-teal-800 font-semibold py-2 px-4 rounded hover:bg-gray-200">Send</button>
+                                    <button onClick={AccountsHref} className="w-2/5 bg-white text-teal-800 font-semibold py-2 px-4 rounded hover:bg-gray-200">Back To Accounts</button>
+                                </div>
+                        </div>
+                    ) : (
+                        <div className="h-full flex flex-col justify-around">
+                            <div>
+                            <div className="w-full py-3">
+                                <div className="relative w-full min-w-[200px] h-10">
+                                    <Select id="selectAccount"
+                                        options={options}
+                                        onChange={handleChange}
+                                        defaultValue={options[0]}
+                                    />
+                                </div>
+                            </div>
+                            <div className="w-full py-3">
+                                <div className="relative w-full min-w-[200px] h-10">
+                                    <input
+                                        value={transferAmount}
+                                        onChange={e => setTransferAmount(e.target.value)}
+                                        className="peer w-full h-full bg-transparent text-white font-normal outline outline-0 focus:outline-0 disabled:bg-blue-gray-50 disabled:border-0 transition-all placeholder-shown:border placeholder-shown:border-white placeholder-shown:border-t-blue-gray-200 border focus:border-2 border-t-transparent focus:border-t-transparent text-sm px-3 py-2.5 rounded-[7px] border-blue-gray-200 focus:border-white"
+                                        placeholder=" "
+                                    />
+                                    <label
+                                        className="flex w-full h-full select-none pointer-events-none absolute left-0 font-normal !overflow-visible truncate peer-placeholder-shown:text-white leading-tight peer-focus:leading-tight peer-disabled:text-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500 transition-all -top-1.5 peer-placeholder-shown:text-sm text-[11px] peer-focus:text-[11px] before:content[' '] before:block before:box-border before:w-2.5 before:h-1.5 before:mt-[6.5px] before:mr-1 peer-placeholder-shown:before:border-transparent before:rounded-tl-md before:border-t peer-focus:before:border-t-2 before:border-l peer-focus:before:border-l-2 before:pointer-events-none before:transition-all peer-disabled:before:border-transparent after:content[' '] after:block after:flex-grow after:box-border after:w-2.5 after:h-1.5 after:mt-[6.5px] after:ml-1 peer-placeholder-shown:after:border-transparent after:rounded-tr-md after:border-t peer-focus:after:border-t-2 after:border-r peer-focus:after:border-r-2 after:pointer-events-none after:transition-all peer-disabled:after:border-transparent peer-placeholder-shown:leading-[3.75] text-white peer-focus:text-white before:border-blue-gray-200 peer-focus:before:!border-white after:border-blue-gray-200 peer-focus:after:!border-white">Amount
+                                    </label>
+                                </div>
+                            </div>
+                            </div>
+                            {transferSuccess === 'True' && (
+                                <div class="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative" role="alert">
+                                    <span class="block sm:inline">{transferMessage}</span>
+                                </div>
+                            )}
+                                <div className="w-full items-center flex justify-evenly py-3">
+                                    <button onClick={CheckTransferValidity} className="w-2/5 bg-white text-teal-800 font-semibold py-2 px-4 rounded hover:bg-gray-200">Send</button>
+                                    <button onClick={AccountsHref} className="w-2/5 bg-white text-teal-800 font-semibold py-2 px-4 rounded hover:bg-gray-200">Back To Accounts</button>
+                                </div>
                         </div>
                     )
                     }
